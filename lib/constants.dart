6bb6a2637fa732dc1a45bfaf233d2831015e49cd
Eugene <evgeniiorlov204@gmail.com>
2021-10-12 21:30:30 +0300

    Created library for constants

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:math';
+
+final random = Random();
+const int colorComponentsRange = 256;
+const int alpha = 255;
+const minFontSize = 20.0;
+const fontSizeRange = 10;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'constants.dart' as constants;
 
 void main() {
   runApp(const MaterialApp(home: Scaffold(body: MyClass())));
@@ -17,11 +17,6 @@ class _CounterState extends State<MyClass> {
   Color textColor = Colors.black;
   int value = 0;
   String randomPictureUrl = "https://picsum.photos/200/300";
-  final random = Random();
-  final int colorComponentsRange = 256;
-  final int alpha = 255;
-  final minFontSize = 20.0;
-  final fontSizeRange = 10;
 
   changeColors() {
     backgroundColor = getRandomColor();
@@ -34,15 +29,16 @@ class _CounterState extends State<MyClass> {
   }
 
   updateImage() {
-    randomPictureUrl="https://picsum.photos/200/300?v=${DateTime.now().millisecondsSinceEpoch}";
+    randomPictureUrl =
+        "https://picsum.photos/200/300?v=${DateTime.now().millisecondsSinceEpoch}";
   }
 
   Color getRandomColor() {
     return Color.fromARGB(
-      alpha,
-      random.nextInt(colorComponentsRange),
-      random.nextInt(colorComponentsRange),
-      random.nextInt(colorComponentsRange),
+      constants.alpha,
+      constants.random.nextInt(constants.colorComponentsRange),
+      constants.random.nextInt(constants.colorComponentsRange),
+      constants.random.nextInt(constants.colorComponentsRange),
     );
   }
 
@@ -74,19 +70,14 @@ class _CounterState extends State<MyClass> {
       child: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: NetworkImage(
-                    randomPictureUrl
-                ),
-                fit: BoxFit.cover
-            )
-        ),
-        // color: backgroundColor,
+                image: NetworkImage(randomPictureUrl), fit: BoxFit.cover)),
         child: Align(
           alignment: Alignment.center,
           child: Text(
             'Hey there!\nYou clicked $value times',
             style: TextStyle(
-                fontSize: random.nextInt(fontSizeRange) + minFontSize,
+                fontSize: constants.random.nextInt(constants.fontSizeRange) +
+                    constants.minFontSize,
                 color: textColor,
                 decoration: TextDecoration.none),
             textDirection: TextDirection.ltr,
